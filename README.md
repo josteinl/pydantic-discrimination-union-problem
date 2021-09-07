915b02e84ffaf9a871a4a55963a38ea5c095cd34
Jostein Leira <jostein@leira.net>
2021-09-07 12:12:40 +0200

    Add small example work for one commit and breaks on a later commit.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # pydantic-discrimination-union-problem
 Example code for testing Fastapi, Pydantic and discrimination union combination
+
+## Installation
+Using Python version 3.8.10. Create a virtual environment and activate it.
+
+### OK
+Uses the last OK commit for this example to work
+[git+git://github.com/PrettyWood/pydantic.git@45db4ad3aa558879824a91dd3b011d0449eb2977](https://github.com/PrettyWood/pydantic/commit/45db4ad3aa558879824a91dd3b011d0449eb2977)
+
+    pip install -r requirements_ok.txt --force-reinstall
+
+### ERROR
+Uses the breaking commit [git+git://github.com/PrettyWood/pydantic.git@417601ae551b92ad875b84dbad93a51eaa40f66c](https://github.com/PrettyWood/pydantic/commit/417601ae551b92ad875b84dbad93a51eaa40f66c).
+
+    pip install -r requirements_error.txt --force-reinstall
+
+## Start server
+With activated virtual environment
+
+    python main.py
+
+Access the openapi endpoint documentation [http://localhost/docs](http://localhost/docs)
+
---
+++ main.py
@@ -0,0 +1,53 @@
+import enum
+from typing import List, Union, Literal
+
+from typing_extensions import Annotated
+from fastapi import FastAPI
+from pydantic import Field, BaseModel
+import uvicorn
+
+
+class CutleryTypeEnum(enum.Enum):
+    KNIFE = 1
+    FORK = 2
+    SPOON = 3
+
+
+class CutleryBase(BaseModel):
+    name: str
+
+    # class Config:
+    #     orm_mode = True
+
+
+class Knife(CutleryBase):
+    cutlery_type_id: Literal[CutleryTypeEnum.KNIFE]
+
+
+class Fork(CutleryBase):
+    cutlery_type_id: Literal[CutleryTypeEnum.FORK]
+    number_of_teeth: int = None
+
+
+class Spoon(CutleryBase):
+    cutlery_type_id: Literal[CutleryTypeEnum.SPOON]
+    volume: float = None
+
+
+Cutlery = Annotated[
+    Union[Knife, Fork, Spoon],
+    Field(discriminator="cutlery_type_id"),
+]
+
+app = FastAPI()
+
+
+@app.get("/cutlery",
+         response_model=List[Cutlery])
+async def get_cutlery():
+    return [{'cutlery_type_id': CutleryTypeEnum.KNIFE, 'name': 'My sharp knife'},
+            {'cutlery_type_id': CutleryTypeEnum.FORK, 'name': 'The three teeth fork'},
+            {'cutlery_type_id': CutleryTypeEnum.SPOON, 'name': 'Tea spoon'}]
+
+
+uvicorn.run(app, host="127.0.0.1", port=80)
